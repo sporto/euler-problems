a02f7dbc90922bfa104081bccb88dc1eea106437
Sebastian Porto <s@porto5.com>
2019-10-09 21:46:14 +1100

    Solve 0015

--- e0015/main.ml
+++ e0015/main.ml
@@ -1,23 +1,35 @@
 open Printf
 
-let grid =
-	2
+(* Start with a list that is double the grid size e.g. 3 x 3 grid -> 6 list 
+All numbers are 0 except the two in the middle, which are 1
+*)
 
-let print_point (x,y) =
-	(printf "(%d, %d)") x y
+let base_list grid_size =
+	List.init
+		(grid_size * 2)
+		(fun ix ->
+			if ix == grid_size - 1 || ix == grid_size then
+				1
+			else
+				0
+		)
 
-let rec go (x, y) =
-	if x > grid then
-		[]
-	else if y > grid then
-		[]
-	else if (x, y) = (grid, grid) then
-		[1]
-	else
-		go (x + 1, y) @ go (x, y + 1)
+(*
+The reduce the list by adding up contiguous cells
+e.g. 1 3 3 1 -> 4 6 4 -> 10 10 -> 20
+*)
+let reduce l =
+	List.map2
+		(+)
+		(l |> List.rev |> List.tl |> List.rev) 
+		(List.tl l)
 
+let rec reduce_until_one l =
+	match l with
+		| [ n ] -> [ n ]
+		| more -> reduce more |> reduce_until_one
 
 let () =
-	go (0,0)
-		|> List.length
-		|> (printf "%d")
+	base_list 20
+		|> reduce_until_one
+		|> List.iter (printf "%d")

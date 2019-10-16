f193952778d3e3388ce764667ac616fe29c96d37
Sebastian Porto <s@porto5.com>
2019-10-16 22:32:44 +1100

    Solve 0016

--- e0016/main.ml
+++ e0016/main.ml
@@ -1,6 +1,11 @@
-open Printf
+(* open Printf *)
+open Core
 
 let () =
 	Big_int.power_int_positive_int 2 1000
 		|> Big_int.string_of_big_int
-		|> (printf "%s")
+		|> String.to_list
+		|> List.map ~f:(String.make 1)
+		|> List.map ~f:int_of_string
+		|> List.fold_left ~init:0 ~f:(+)
+		|> (printf "%i")

f01991b7e341b44eaba95b3f42d88f6c85513096
Sebastian Porto <s@porto5.com>
2019-11-04 18:47:04 +1100

    Add functions to walk the tree

--- e0018/main.ml
+++ e0018/main.ml
@@ -1,5 +1,7 @@
 (* open Printf *)
 open Core
+(* open Core.List *)
+open Core.Option
 
 let file_name =
 	"input.txt"
@@ -21,7 +23,22 @@ let parse_file (): int list list =
 	read_file ()
 		|> List.map ~f:parse_line
 
+let left_from (matrix: int list list) (row_ix: int) (col_ix: int): int option =
+	List.nth matrix (row_ix + 1)
+		>>= (fun row -> List.nth row col_ix)
+
+let right_from (matrix: int list list) (row_ix: int) (col_ix: int): int option =
+	List.nth matrix (row_ix + 1)
+		>>= (fun row -> List.nth row (col_ix + 1))
+
 let () =
-	parse_file ()
-		|> List.concat
-		|> List.iter ~f:(printf "%i")
+	let
+		matrix =
+			parse_file ()
+	in
+	right_from matrix 1 1
+		|> Option.value ~default:0
+		|> printf "%i"
+
+		(* |> List.concat *)
+		(* |> List.iter ~f:(printf "%i") *)

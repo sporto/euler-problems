4c392dacb62a10f91e53057ef8a1dff5e6c1f4a1
Sebastian Porto <s@porto5.com>
2019-11-05 17:06:55 +1100

    Try real input

--- e0018/main.ml
+++ e0018/main.ml
@@ -1,10 +1,8 @@
-(* open Printf *)
 open Core
-(* open Core.List *)
 open Core.Option
 
 let file_name =
-	"input.txt"
+	"input_1.txt"
 
 let read_file () : string list =
 	file_name
@@ -23,22 +21,52 @@ let parse_file (): int list list =
 	read_file ()
 		|> List.map ~f:parse_line
 
-let left_from (matrix: int list list) (row_ix: int) (col_ix: int): int option =
-	List.nth matrix (row_ix + 1)
-		>>= (fun row -> List.nth row col_ix)
+let get matrix (x, y) =
+	List.nth matrix (y)
+		>>= (fun row -> List.nth row x)
 
-let right_from (matrix: int list list) (row_ix: int) (col_ix: int): int option =
-	List.nth matrix (row_ix + 1)
-		>>= (fun row -> List.nth row (col_ix + 1))
+let left_coor (x , y) =
+	(x, y + 1)
+
+let right_coor (x, y) =
+	(x + 1, y  + 1)
+
+let left_value (matrix: int list list) coor : int option =
+	get matrix (left_coor coor)
+
+let right_value (matrix: int list list) coor : int option =
+	get matrix (right_coor coor)
+
+let rec add (matrix: int list list) (acc: int) coor: int =
+	match get matrix coor with
+	| Some n ->
+		pick_side matrix (acc + n) coor
+	| _ ->
+		acc
+and pick_side (matrix: int list list) (acc: int) coor =
+	let
+		left =
+			left_value matrix coor
+	in
+	let
+		right =
+			right_value matrix coor
+	in
+	match (left, right) with
+	| (Some l, Some r) ->
+		if l > r then
+			add matrix acc (left_coor coor)
+		else
+			add matrix acc (right_coor coor)
+	| _ ->
+		acc
+
+
 
 let () =
 	let
 		matrix =
 			parse_file ()
 	in
-	right_from matrix 1 1
-		|> Option.value ~default:0
+	add matrix 0 (0, 0)
 		|> printf "%i"
-
-		(* |> List.concat *)
-		(* |> List.iter ~f:(printf "%i") *)

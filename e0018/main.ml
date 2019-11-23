af4e2f0aa32a72e71f60ef32cc9d9327dd0b94a8
Sebastian Porto <s@porto5.com>
2019-11-23 17:53:19 +1100

    Change ocaml version

--- e0018/main.ml
+++ e0018/main.ml
@@ -1,5 +1,8 @@
 open Core
-open Core.Option
+(* open Core.Option *)
+(* open Core.List *)
+
+(* Read input *)
 
 let file_name =
 	"input_1.txt"
@@ -21,9 +24,14 @@ let parse_file (): int list list =
 	read_file ()
 		|> List.map ~f:parse_line
 
-let get matrix (x, y) =
-	List.nth matrix (y)
-		>>= (fun row -> List.nth row x)
+(* Helpers to get values in the matrix *)
+
+let get (matrix: int list list) ((x , y) : int * int) : int option =
+	match List.nth matrix y with
+		| None ->
+			None
+		| Some row ->
+			List.nth row x
 
 let left_coor (x , y) =
 	(x, y + 1)
@@ -34,39 +42,67 @@ let right_coor (x, y) =
 let left_value (matrix: int list list) coor : int option =
 	get matrix (left_coor coor)
 
+let left_value_or matrix coor default =
+	left_value matrix coor
+		|> Option.value ~default: default
+
 let right_value (matrix: int list list) coor : int option =
 	get matrix (right_coor coor)
 
-let rec add (matrix: int list list) (acc: int) coor: int =
-	match get matrix coor with
-	| Some n ->
-		pick_side matrix (acc + n) coor
-	| _ ->
-		acc
-and pick_side (matrix: int list list) (acc: int) coor =
-	let
-		left =
-			left_value matrix coor
-	in
-	let
-		right =
-			right_value matrix coor
-	in
-	match (left, right) with
-	| (Some l, Some r) ->
-		if l > r then
-			add matrix acc (left_coor coor)
-		else
-			add matrix acc (right_coor coor)
-	| _ ->
-		acc
+let right_value_or matrix coor (default: int) : int =
+	right_value matrix coor
+		|> Option.value ~default: default
 
 
+(* Walk the tree *)
+
+let max_for_coor matrix coor =
+	let coor_val =
+		get matrix coor
+			|> Option.value ~default: 0
+	in
+	let left =
+		left_value_or matrix coor 0
+	in
+	let right =
+		right_value_or matrix coor 0
+	in
+	let max_child =
+		max left right
+	in
+	coor_val + max_child
+
+(* Take the last two rows and calculate the max on each *)
+
+let reduce_rows (two_rows: int list list) : int list=
+	match two_rows with
+	| [] -> []
+	| r1 :: _ ->
+		List.range 0 ((List.length r1) - 1)
+			|> List.map ~f: (fun x -> max_for_coor two_rows (x, 0))
+
+let rec reduce (matrix : int list list) : int list =
+	let reversed_matrix =
+			List.rev matrix
+	in
+	let debug =
+		matrix
+			|> List.map
+				(List.iter ~f:(printf "%d"))
+	in
+	match reversed_matrix with
+	| [] ->
+		[]
+	| last :: [] ->
+		last
+	| last :: second_last :: rest ->
+		List.append (List.rev rest) [reduce_rows [second_last; last]]
+			|> reduce
 
 let () =
 	let
 		matrix =
 			parse_file ()
 	in
-	add matrix 0 (0, 0)
-		|> printf "%i"
+	reduce matrix
+		|> List.iter ~f:(printf "%d ")
